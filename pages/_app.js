7229dbfd163247daa939c9e6bc42a8ca84567f27
Rithikeshh <rithikeshh@gmail.com>
2023-09-22 05:05:56 +0530

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,22 @@
 import React from 'react';
 
 import MyComponent from '../components/MyComponent';
+import {ThemeProvider} from '../contexts/ThemeContext';
+import {LanguageProvider} from '../contexts/LanguageContext';
+import {UserProvider} from '../contexts/UserContext';
+import {ConfigProvider} from '../contexts/ConfigContext';
 
 function App() {
   return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <UserProvider>
+          <ConfigProvider>
             <MyComponent />
+          </ConfigProvider>
+        </UserProvider>
+      </LanguageProvider>      
+    </ThemeProvider>
   );
 }
 
